b7dac7497b8a8a21ab57d36cd1f5e8de8a87a95f
Yahor Barkouski <yahor.barkouski@gmail.com>
2023-08-12 01:10:42 +0200

    chore: change docker image name to kotlin-compiler

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -2,4 +2,4 @@
 
 kotlin_version=$(awk '{ if ($1=="systemProp.kotlinVersion") { print $2; } }' FS='=' ./gradle.properties)
 
-docker build . --file Dockerfile --tag my-image-name:$(date +%s) --build-arg KOTLIN_VERSION=$kotlin_version
+docker build . --file Dockerfile --tag kotlin-compiler:$(date +%s) --build-arg KOTLIN_VERSION=$kotlin_version
